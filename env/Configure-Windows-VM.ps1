d40e06f87e05c14fbba26fe83c8e7852875e1384
Sachi Williamson <sachi.williamson@nwcadence.com>
2017-06-15 15:23:40 -0700

    fix PS1 and pass args from template

--- env/Configure-Windows-VM.ps1
+++ env/Configure-Windows-VM.ps1
@@ -1,3 +1,8 @@
+param
+(
+    [string]$workloads
+)
+
 # Set PowerShell execution policy
 Set-ExecutionPolicy RemoteSigned -Force
 
@@ -20,11 +25,23 @@ refreshenv
 & choco install googlechrome -y
 & choco install poshgit -y
 & choco install dotnet4.6.1 -y
-#& choco install windowsazurepowershell -y
 & choco install nodejs -y
-& choco install visualstudio2017-installer -y
-& choco install visualstudio2017enterprise --version 15.2.26430.20170605 --package-parameters "--add Microsoft.VisualStudio.Component.TestTools.CodedUITest	Microsoft.VisualStudio.Component.TestTools.WebLoadTest Microsoft.VisualStudio.Component.TestTools.Core Microsoft.VisualStudio.Workload.NetWeb;includeRecommended;includeOptional Microsoft.VisualStudio.Workload.Azure;includeRecommended Microsoft.VisualStudio.Workload.Data;includeRecommended Microsoft.VisualStudio.Workload.ManagedDesktop;includeRecommended;includeOptional Microsoft.VisualStudio.Workload.NetCoreTools --passive --locale en-US" -y
+
+$vs_EnterpriseUrl = "https://aka.ms/vs/15/release/vs_enterprise.exe"
+$vs_Enterprise = "$($env:TEMP)\vs_Enterprise.exe"
+
+Invoke-WebRequest $vs_EnterpriseUrl -OutFile $vs_Enterprise
+
+$workloads -split ",[ ]{0,1}" | ForEach-Object {
+    
+    $workloadArgs += "--add $($_) "
+}
+
+$workloadArgs += "--passive --norestart --wait"
+$workloadParams = $workloadArgs.Split(" ")
+Start-Process $vs_Enterprise $workloadParams -Wait
 
 refreshenv
 
-Restart-Computer -Wait 60
+Start-Sleep -Seconds 60
+Restart-Computer
