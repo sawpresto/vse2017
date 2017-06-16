3c72eb4d7956ec59e695b3ac5859a918a0d58a19
Sachi Williamson <sachi.williamson@nwcadence.com>
2017-06-16 12:19:31 -0700

    update ps1

--- env/Configure-Windows-VM.ps1
+++ env/Configure-Windows-VM.ps1
@@ -17,8 +17,9 @@ refreshenv
 & choco install dotnet4.6.1 -y
 #& choco install nodejs -y
 
+New-Item "C:\Temp" -Type Directory
 $vs_EnterpriseUrl = "https://aka.ms/vs/15/release/vs_enterprise.exe"
-$vs_Enterprise = "$($env:TEMP)\vs_Enterprise.exe"
+$vs_Enterprise = "C:\Temp\vs_Enterprise.exe"
 
 Invoke-WebRequest $vs_EnterpriseUrl -OutFile $vs_Enterprise
 
@@ -27,7 +28,7 @@ $workloads -split ",[ ]{0,1}" | ForEach-Object {
     $workloadArgs += "--add $($_) "
 }
 
-$workloadArgs += "--passive --norestart --wait"
+$workloadArgs += "--quiet --norestart --wait"
 $workloadParams = $workloadArgs.Split(" ")
 Start-Process $vs_Enterprise $workloadParams -Wait
 
