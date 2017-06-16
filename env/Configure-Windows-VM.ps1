7c1ae906566ca3279e2d43e13aee7573e2c0c406
Sachi Williamson <sachi.williamson@nwcadence.com>
2017-06-16 11:57:50 -0700

    remove certs in ps1 script

--- env/Configure-Windows-VM.ps1
+++ env/Configure-Windows-VM.ps1
@@ -11,23 +11,13 @@ iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 
 refreshenv
 
-# Install certificates
-& certmgr -add -c certificates\manifestSignCertificates.p12 -n "Microsoft Code Signing PCA 2011" -s -r LocalMachine CA
-& certmgr -add -c certificates\manifestSignCertificates.p12 -n "Microsoft Root Certificate Authority" -s -r LocalMachine root
-& certmgr -add -c certificates\manifestCounterSignCertificates.p12 -n "Microsoft Time-Stamp PCA 2010" -s -r LocalMachine CA
-& certmgr -add -c certificates\manifestCounterSignCertificates.p12 -n "Microsoft Root Certificate Authority" -s -r LocalMachine root
-& certmgr -add -c certificates\vs_installer_opc.SignCertificates.p12 -n "Microsoft Code Signing PCA" -s -r LocalMachine CA
-& certmgr -add -c certificates\vs_installer_opc.SignCertificates.p12 -n "Microsoft Root Certificate Authority" -s -r LocalMachine root
-
-refreshenv
-
 # Install Chocolatey packages
 #& choco install googlechrome -y
 #& choco install poshgit -y
 & choco install dotnet4.6.1 -y
 #& choco install nodejs -y
 
-<#$vs_EnterpriseUrl = "https://aka.ms/vs/15/release/vs_enterprise.exe"
+$vs_EnterpriseUrl = "https://aka.ms/vs/15/release/vs_enterprise.exe"
 $vs_Enterprise = "$($env:TEMP)\vs_Enterprise.exe"
 
 Invoke-WebRequest $vs_EnterpriseUrl -OutFile $vs_Enterprise
@@ -44,4 +34,4 @@ Start-Process $vs_Enterprise $workloadParams -Wait
 refreshenv
 
 Start-Sleep -Seconds 60
-Restart-Computer#>
+Restart-Computer
