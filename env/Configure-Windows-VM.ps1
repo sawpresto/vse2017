b895349ca221611fa34f81c00cf4ebf7f0f31668
Sachi Williamson <sachi.williamson@nwcadence.com>
2017-06-16 16:31:17 -0700

    switch order of choco

--- env/Configure-Windows-VM.ps1
+++ env/Configure-Windows-VM.ps1
@@ -6,24 +6,10 @@ param
 # Set PowerShell execution policy
 #Set-ExecutionPolicy RemoteSigned -Force
 
-# Install Chocolatey
-#iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
-
-refreshenv
-
-# Install Chocolatey packages
-<#& choco install googlechrome -y
-& choco install firefox -y
-& choco install poshgit -y
-& choco install dotnet4.6.1 -y
-& choco install nodejs -y#>
-
 $vs_EnterpriseUrl = "https://aka.ms/vs/15/release/vs_enterprise.exe"
 $vs_Enterprise = "C:\Windows\Temp\vs_Enterprise.exe"
 
 Invoke-WebRequest $vs_EnterpriseUrl -OutFile $vs_Enterprise
-
-Start-Sleep -Seconds 30
 
 $workloads.Split(",[ ]{0,1}") | ForEach-Object {
     
@@ -33,6 +19,17 @@ $workloads.Split(",[ ]{0,1}") | ForEach-Object {
 $workloadArgs += "--quiet --norestart --wait"
 $workloadParams = $workloadArgs.Split(" ")
 Start-Process $vs_Enterprise $workloadParams -Wait
+
+# Install Chocolatey
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+refreshenv
+
+# Install Chocolatey packages
+& choco install googlechrome -y
+& choco install firefox -y
+& choco install poshgit -y
+& choco install nodejs -y
 
 refreshenv
 
