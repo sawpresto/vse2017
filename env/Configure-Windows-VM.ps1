081d2f88a17e02531cefc51702ac92d25ede3380
Sachi Williamson <sachi.williamson@nwcadence.com>
2017-06-16 15:01:45 -0700

    update ps1

--- env/Configure-Windows-VM.ps1
+++ env/Configure-Windows-VM.ps1
@@ -1,4 +1,7 @@
-$workloads = "Microsoft.VisualStudio.Component.TestTools.CodedUITest"
+param
+(
+    [string]$workloads = "Microsoft.VisualStudio.Component.TestTools.CodedUITest,Microsoft.VisualStudio.Component.TestTools.WebLoadTest,Microsoft.VisualStudio.Component.TestTools.Core,Microsoft.VisualStudio.Workload.Azure;includeRecommended,Microsoft.VisualStudio.Workload.NetWeb;includeRecommended;includeOptional,Microsoft.VisualStudio.Workload.Data;includeRecommended,Microsoft.VisualStudio.Workload.ManagedDesktop;includeRecommended;includeOptional,Microsoft.VisualStudio.Workload.NetCoreTools"
+)
 
 # Set PowerShell execution policy
 #Set-ExecutionPolicy RemoteSigned -Force
@@ -9,15 +12,18 @@ iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 refreshenv
 
 # Install Chocolatey packages
-#& choco install googlechrome -y
-#& choco install poshgit -y
+& choco install googlechrome -y
+& choco install firefox -y
+& choco install poshgit -y
 & choco install dotnet4.6.1 -y
-#& choco install nodejs -y
+& choco install nodejs -y
 
 $vs_EnterpriseUrl = "https://aka.ms/vs/15/release/vs_enterprise.exe"
 $vs_Enterprise = "C:\Windows\Temp\vs_Enterprise.exe"
 
 Invoke-WebRequest $vs_EnterpriseUrl -OutFile $vs_Enterprise
+
+Start-Sleep -Seconds 30
 
 $workloads.Split(",[ ]{0,1}") | ForEach-Object {
     
